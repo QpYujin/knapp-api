ea861328487632733f5a5d2194d591df89abab5c
avalluru <Cloudnine11>
2017-01-06 17:00:39 -0600

    Added application db user

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,9 @@
+-- Application user
+CREATE DATABASE knapp;
+CREATE USER 'knappuser'@'localhost' IDENTIFIED BY 'knappuser';
+GRANT ALL ON knapp.* TO 'knappuser'@'localhost';
+
+-- Application test user
 CREATE DATABASE knapptest;
-
 CREATE USER 'knapptestuser'@'localhost' IDENTIFIED BY 'knapptestuser';
-
 GRANT ALL ON knapptest.* TO 'knapptestuser'@'localhost';
