626a35072ed0e315c63c7f5bdce32fe8108e5c65
avalluru <Cloudnine11>
2017-01-06 16:58:36 -0600

    Updated Test class to use test application properties file

--- src/test/java/com/questioner/knapp/api/controllers/QTypeControllerTests.java
+++ src/test/java/com/questioner/knapp/api/controllers/QTypeControllerTests.java
@@ -11,7 +11,9 @@ import static org.junit.Assert.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -26,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:/application-test.properties")
 public class QTypeControllerTests {
 
     @Autowired
